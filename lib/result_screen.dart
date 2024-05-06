382b0de643fb0a122880970a2fe7108697f6cccf
Zazzaro <sassaro01cristian@gmail.com>
2024-05-06 16:53:25 +0200

    Added ResultScreen widget with getSummaryData method to generate summary data based on chosen answers

--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app_flutter/data/questions.dart';
 
 class ResultScreen extends StatelessWidget {
-  const ResultScreen({super.key});
+  const ResultScreen({super.key, required this.chosenAnswers});
+
+  final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      //  loop body
+      summary.add({
+        'question_index': i,
+        'question': questions[i],
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswers[i]
+      });
+    }
+    ;
+
+    return summary;
+  }
+
   @override
   Widget build(context) {
     return SizedBox(
