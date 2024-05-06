919f082871f20364e7e25538ee0efdf961ddeef3
Zazzaro <sassaro01cristian@gmail.com>
2024-05-06 13:30:04 +0200

    changed logic to dispay qyestions buttons dinamically

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app_flutter/answer_button.dart';
+import 'package:quiz_app_flutter/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,30 +11,31 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+  void aAnswerQuestion() {
+    // 3 way of acheving the same result
+    // currentQuestionIndex = currentQuestionIndex + 1;
+    // currentQuestionIndex += 1;
+    currentQuestionIndex++;
+  }
+
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'the question...',
-            style: TextStyle(
-                color: Colors.white, fontSize: 29, fontWeight: FontWeight.bold),
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(
+                color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
           ),
-          const SizedBox(
-            height: 30,
-          ),
-          AnswerButton(answerText: 'answer text1', onTap: () {}),
-          const SizedBox(
-            height: 15,
-          ),
-          AnswerButton(answerText: 'answer text2', onTap: () {}),
-          const SizedBox(
-            height: 15,
-          ),
-          AnswerButton(answerText: 'answer text3', onTap: () {}),
+          const SizedBox(height: 30),
+          ...currentQuestion.getShuffledAnswers().map((answer) {
+            return AnswerButton(answerText: answer, onTap: () {});
+          })
         ],
       ),
     );
