e6614853dbb54a32ecf76704a85b59004fd2ab48
Zazzaro <sassaro01cristian@gmail.com>
2024-05-06 13:29:13 +0200

    added style and container t buttons

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -10,15 +10,25 @@ class AnswerButton extends StatelessWidget {
   final void Function() onTap;
   @override
   Widget build(context) {
-    return ElevatedButton(
-      onPressed: onTap,
-      style: ElevatedButton.styleFrom(
-        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-        backgroundColor: Color.fromARGB(255, 126, 0, 252),
-        foregroundColor: Colors.white,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6)),
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 0, horizontal: 40),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          ElevatedButton(
+            onPressed: onTap,
+            style: ElevatedButton.styleFrom(
+              padding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+              backgroundColor: Color.fromARGB(255, 126, 0, 252),
+              foregroundColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(6)),
+            ),
+            child: Text(answerText),
+          ),
+          const SizedBox(height: 20),
+        ],
       ),
-      child: Text(answerText),
     );
   }
 }
