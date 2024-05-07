70fd73dbc5b9239dcd7bae4c03200dee5df18094
Zazzaro <sassaro01cristian@gmail.com>
2024-05-07 12:09:27 +0200

    changed the position of the start button icon

--- lib/start_button_widget.dart
+++ lib/start_button_widget.dart
@@ -8,28 +8,38 @@ class StartButton extends StatelessWidget {
   final void Function() startQuiz;
 
   @override
-  Widget build(BuildContext context) => ElevatedButton.icon(
-        // Trigger the startQuiz function when the button is pressed
-        onPressed: startQuiz,
-        style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all<Color>(
-              const Color.fromARGB(255, 0, 8, 255)),
-          shape:
-              MaterialStateProperty.all<OutlinedBorder>(RoundedRectangleBorder(
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: startQuiz,
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all<Color>(
+          const Color.fromARGB(255, 0, 8, 255),
+        ),
+        shape: MaterialStateProperty.all<OutlinedBorder>(
+          RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(16),
-          )),
+          ),
         ),
-        // Icon displayed on the button
-        icon: const Icon(
-          Icons.arrow_forward_ios_rounded,
-        ),
-        // Text label displayed on the button
-        label: const Text(
-          'Start Quiz',
-          style: TextStyle(
+      ),
+      child: const Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Start Quiz',
+            style: TextStyle(
               color: Colors.white,
               fontSize: 22,
-              fontWeight: FontWeight.bold), // Text styling
-        ),
-      );
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(width: 5), // Adjust the spacing between icon and text
+          Icon(
+            Icons.arrow_forward_ios_rounded,
+            color: Colors.white,
+          ),
+        ],
+      ),
+    );
+  }
 }
